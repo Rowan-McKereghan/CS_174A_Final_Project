a802927ddb70d3d87dd5ad4a8ee263f04fd15dba
Ethan Jackson <ethantjackson44@gmail.com>
2021-11-20 20:16:50 -0800

    add object reader class

--- common.js
+++ common.js
@@ -140,28 +140,35 @@ const Cube_Outline = (defs.Cube_Outline = class Cube_Outline extends Shape {
   }
 });
 
-const Square_Pyramid_Outline = (defs.Square_Pyramid_Outline = class Square_Pyramid_Outline extends Shape {
-  constructor() {
-    super('position', 'color');
+const Square_Pyramid_Outline =
+  (defs.Square_Pyramid_Outline = class Square_Pyramid_Outline extends Shape {
+    constructor() {
+      super('position', 'color');
 
-    this.arrays.position = Vector3.cast(
-      [0, 1, 0], [-1, -1, -1],
-      [0, 1, 0], [-1, -1, 1],
-      [0, 1, 0], [1, -1, -1],
-      [0, 1, 0], [1, -1, 1],
-      [-1, -1, -1], [-1, -1, 1],
-      [-1, -1, -1], [1, -1, -1],
-      [1, -1, 1], [-1, -1, 1],
-      [1, -1, 1], [1, -1, -1]
-    );
+      this.arrays.position = Vector3.cast(
+        [0, 1, 0],
+        [-1, -1, -1],
+        [0, 1, 0],
+        [-1, -1, 1],
+        [0, 1, 0],
+        [1, -1, -1],
+        [0, 1, 0],
+        [1, -1, 1],
+        [-1, -1, -1],
+        [-1, -1, 1],
+        [-1, -1, -1],
+        [1, -1, -1],
+        [1, -1, 1],
+        [-1, -1, 1],
+        [1, -1, 1],
+        [1, -1, -1]
+      );
 
-    for (let i = 0; i < 16; i++) {
-      this.arrays.color.push(color(1, 1, 1, 1));
+      for (let i = 0; i < 16; i++) {
+        this.arrays.color.push(color(1, 1, 1, 1));
+      }
     }
-  }
-});
-
-
+  });
 
 const Basic_Shader = (defs.Basic_Shader = class Basic_Shader extends Shader {
   // **Basic_Shader** is nearly the simplest example of a subclass of Shader, which stores and
@@ -406,5 +413,77 @@ const Phong_Shader = (defs.Phong_Shader = class Phong_Shader extends Shader {
 
     this.send_material(context, gpu_addresses, material);
     this.send_gpu_state(context, gpu_addresses, gpu_state, model_transform);
+  }
+});
+
+const Textured_Phong = (defs.Textured_Phong = class Textured_Phong extends (
+  Phong_Shader
+) {
+  // **Textured_Phong** is a Phong Shader extended to addditionally decal a
+  // texture image over the drawn shape, lined up according to the texture
+  // coordinates that are stored at each shape vertex.
+  vertex_glsl_code() {
+    // ********* VERTEX SHADER *********
+    return (
+      this.shared_glsl_code() +
+      `
+                varying vec2 f_tex_coord;
+                attribute vec3 position, normal;                            
+                // Position is expressed in object coordinates.
+                attribute vec2 texture_coord;
+                
+                uniform mat4 model_transform;
+                uniform mat4 projection_camera_model_transform;
+        
+                void main(){                                                                   
+                    // The vertex's final resting place (in NDCS):
+                    gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
+                    // The final normal vector in screen space.
+                    N = normalize( mat3( model_transform ) * normal / squared_scale);
+                    vertex_worldspace = ( model_transform * vec4( position, 1.0 ) ).xyz;
+                    // Turn the per-vertex texture coordinate into an interpolated variable.
+                    f_tex_coord = texture_coord;
+                  } `
+    );
+  }
+
+  fragment_glsl_code() {
+    // ********* FRAGMENT SHADER *********
+    // A fragment is a pixel that's overlapped by the current triangle.
+    // Fragments affect the final image or get discarded due to depth.
+    return (
+      this.shared_glsl_code() +
+      `
+                varying vec2 f_tex_coord;
+                uniform sampler2D texture;
+        
+                void main(){
+                    // Sample the texture image in the correct place:
+                    vec4 tex_color = texture2D( texture, f_tex_coord );
+                    if( tex_color.w < .01 ) discard;
+                                                                             // Compute an initial (ambient) color:
+                    gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
+                                                                             // Compute the final color with contributions from lights:
+                    gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
+                  } `
+    );
+  }
+
+  update_GPU(context, gpu_addresses, gpu_state, model_transform, material) {
+    // update_GPU(): Add a little more to the base class's version of this method.
+    super.update_GPU(
+      context,
+      gpu_addresses,
+      gpu_state,
+      model_transform,
+      material
+    );
+
+    if (material.texture && material.texture.ready) {
+      // Select texture unit 0 for the fragment shader Sampler2D uniform called "texture":
+      context.uniform1i(gpu_addresses.texture, 0);
+      // For this draw, use the texture image from correct the GPU buffer:
+      material.texture.activate(context);
+    }
   }
 });
